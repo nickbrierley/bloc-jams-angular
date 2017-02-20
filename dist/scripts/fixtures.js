dfc35f54f2e22f3b63c8311775d69c35448a12a2
Nick Brierley <Nick Brierley>
2017-02-20 13:34:21 -0800

    checkpoint5: completed

---
+++ dist/scripts/fixtures.js
@@ -0,0 +1,49 @@
+var albumPicasso = {
+    title: 'The Colors',
+    artist: 'Pablo Picasso',
+    label: 'Cubism',
+    year: '1881',
+    albumArtUrl: 'assets/images/album_covers/01.png',
+    songs: [
+        { title: 'Blue', duration: '161.71', audioUrl: 'assets/music/blue' },
+        { title: 'Green', duration: '103.96', audioUrl: 'assets/music/green' },
+        { title: 'Red', duration: '268.45', audioUrl: 'assets/music/red' },
+        { title: 'Pink', duration: '153.14', audioUrl: 'assets/music/pink' },
+        { title: 'Magenta', duration: '374.22', audioUrl: 'assets/music/magenta'}
+    ]
+};
+ 
+var albumMarconi = {
+    title: 'The Telephone',
+    artist: 'Guglielmo Marconi',
+    label: 'EM',
+    year: '1909',
+    albumArtUrl: 'assets/images/album_covers/20.png',
+    songs: [
+        { title: 'Hello, Operator?', duration: '1:01' },
+        { title: 'Ring, ring, ring', duration: '5:01' },
+        { title: 'Fits in your pocket', duration: '3:21'},
+        { title: 'Can you hear me now?', duration: '3:14' },
+        { title: 'Wrong phone number', duration: '2:15'}
+    ]
+};
+
+var albumPat = {
+    title: 'Probably Nothing, Possibly Everything',
+    artist: 'Pat The Bunny',
+    label: 'Punk',
+    year: '2014',
+    albumArtUrl: 'assets/images/album_covers/pat.jpg',
+    songs: [
+        { title: 'Im Going Home', duration: '148', audioUrl: 'assets/music/01'  },
+        { title: 'The Hand You Reach Out Is Empty, as Mine Is', duration: '154', audioUrl: 'assets/music/02'  },
+        { title: 'Im Not a Good Person', duration: '174', audioUrl: 'assets/music/03' },
+        { title: 'We Were Young Once', duration: '133', audioUrl: 'assets/music/04' },
+        { title: 'Run From Whats Comfortable', duration: '177', audioUrl: 'assets/music/05' },
+        { title: 'Take Me by The Hand and Lead Me Through This Disaster', duration: '126', audioUrl: 'assets/music/06'  },
+        { title: 'Make Total Destroy', duration: '170', audioUrl: 'assets/music/07' },
+        { title: 'We Dont Get Tired, We Get Even', duration: '197', audioUrl: 'assets/music/08'  },
+        { title: 'The Club Hits of Today Will Be the Show Tunes of Tomorrow cover', duration: '71', audioUrl: 'assets/music/09'  },
+        { title: 'A Glorious Shipwreck', duration: '239', audioUrl: 'assets/music/10'}
+    ]
+};
