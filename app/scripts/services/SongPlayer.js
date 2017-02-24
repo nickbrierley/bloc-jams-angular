f8ff29f4c7f89c471b4660710f80e9c084925319
Nick Brierley <Nick Brierley>
2017-02-24 09:52:30 -0800

    checkpoint7-assignment: completed

--- app/scripts/services/SongPlayer.js
+++ app/scripts/services/SongPlayer.js
@@ -1,7 +1,10 @@
 (function() {
     function SongPlayer() {
         var SongPlayer = {};
-        
+        /*
+        @desc Currently playing song
+        @type {Object}
+        */
         var currentSong = null;
         /*
         @desc Buzz object audio file
@@ -28,21 +31,39 @@
             currentSong = song;
         };
         
+        /*
+        @function playSong
+        @desc Playes a song and sets song.playing to true
+        @param {object} song
+        */
+        var playSong = function(song) {
+            currentBuzzObject.play();
+            song.playing = true;
+        }
+        
+        /*
+        @function SongPlayer.play
+        @desc Checks to see if currentSong is the same as the song put in. If it isn't it plays the passed in song and sets it to true. If it is the same and if it's paused it plays it.
+        @param {Object} song
+        */
         SongPlayer.play = function(song) {
             if (currentSong !== song) {
                 
                 setSong(song);
-                currentBuzzObject.play();
-                song.playing = true;
+                playSong(song);
             
             } else if (currentSong === song) {
                 if (currentBuzzObject.isPaused()) {
-                    currentBuzzObject.play();
-                    song.playing = true;
+                    playSong(song);
                 }
             }
         };
         
+        /*
+        @function SongPlayer.pause
+        @desc It pauses the currently playing song and sets song.playing to false
+        @param {Object} song
+        */
         SongPlayer.pause = function(song) {
             currentBuzzObject.pause();
             song.playing = false;
